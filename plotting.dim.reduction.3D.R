f2339a414ad42607b3bdd884153b6de7ee2b8726
Abel Vertesy <vertesy@users.noreply.github.com>
2020-03-21 19:15:58 +0100

    Functions to calculate, store and recall 2D and 3D umap, tSNE, or PCA representations.

--- plotting.dim.reduction.3D.R
+++ plotting.dim.reduction.3D.R
@@ -6,6 +6,7 @@
 
 # Requirements ------------------------
 library(plotly)
+library(MarkdownReportsDev)
 # May also require
 # try (source ('~/GitHub/TheCorvinas/R/CodeAndRoll.R'),silent= F) # generic utilities funtions
 # require('MarkdownReportsDev') # require("devtools") # plotting related utilities functions # devtools::install_github(repo = "vertesy/MarkdownReportsDev")
@@ -13,7 +14,6 @@ library(plotly)
 
 # ------------------------------------------------------------------------
 # ------------------------------------------------------------------------
-
 plot3D.umap <- function(obj=combined.obj, # Plot a 3D umap based on one of the metadata columns. Uses plotly. Based on github.com/Dragonmasterx87.
   category="v.project", AutoAnnotByCluster=c(FALSE, category, "integrated_snn_res.0.7")[3]) {
   stopifnot(category %in% colnames(obj@meta.data))
@@ -59,4 +59,42 @@ plot3D.umap <- function(obj=combined.obj, # Plot a 3D umap based on one of the m
 
 
 # ------------------------------------------------------------------------
+BackupUMAP <- function(obj = combined.obj, dim=2, reduction="umap") { # Backup UMAP to `obj@misc$reductions.backup` from `obj@reductions$umap`.
+  dslot=p0(reduction,dim,"d")
+  obj@misc$reductions.backup[[dslot]] <- obj@reductions$umap
+  return(obj)
+}
+# Example
+# obj <- BackupUMAP(obj = obj, dim=2, reduction=umap"")
+
+# ------------------------------------------------------------------------
+Setup2and3Dumap <- function(obj = combined.obj, nPCs = p$'n.PC', dimensions=3:2, reduction="umap") { # Calculate N-to-K dimensional umaps (default = 2:3); and back them up UMAP to `obj@misc$reductions.backup` from @reductions$umap
+  red <- reduction
+  for (d in dimensions) {
+    iprint(d, "dimensional", red, "is calculated")
+    obj <- RunUMAP(obj, dims = 1:nPCs, n.components = d)
+    obj <- BackupUMAP(obj = obj, dim=d, reduction=red)
+  }
+  return(obj)
+}
+# Example
+# combined.obj <- Setup2and3Dumap(obj = combined.obj, nPCs = p$'n.PC', dimensions=2:3, reduction="umap")
+# qUMAP()
+
+
+# ------------------------------------------------------------------------
+RecallUMAP <- function(obj = combined.obj, dim=2, reduction="umap") { # Set active UMAP to `obj@reductions$umap` from `obj@misc$reductions.backup`.
+  dslot=p0(reduction,dim,"d")
+  iprint(dim, "dimensional", reduction, "is set active. Source")
+  obj@reductions$umap <- obj@misc$reductions.backup[[dslot]]
+  return(obj)
+}
+
+# Example
+# combined.obj <- RecallUMAP(obj = combined.obj, dim=2, reduction="umap")
+# qUMAP()
+# combined.obj <- RecallUMAP(obj = combined.obj, dim=3, reduction="umap")
+# qUMAP()
+
+
 # ------------------------------------------------------------------------
