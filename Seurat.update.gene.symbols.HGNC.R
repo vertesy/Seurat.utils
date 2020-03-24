ada0df3bf85b42219511d79c80953b4129256889
Abel Vertesy <vertesy@users.noreply.github.com>
2020-03-24 15:15:25 +0100

    New functions GetUpdateStats() PlotUpdateStats() HGNC.EnforceUnique()

--- Seurat.update.gene.symbols.HGNC.R
+++ Seurat.update.gene.symbols.HGNC.R
@@ -2,7 +2,7 @@
 # Seurat.update.gene.symbols.HGNC.R
 ######################################################################
 # source ('~/GitHub/Seurat.utils/Seurat.update.gene.symbols.HGNC.R')
-
+require(HGNChelper)
 
 # updateHGNC helper ------------------------------------------------------------------------------------
 RenameGenesSeurat <- function(SeuObj = ls.Seurat[[i]], newnames = HGNC.updated[[i]]) { # Replace gene names in different slots of a Seurat object. Run this before integration. It only changes SeuObj@assays$RNA@counts, @data and @scale.data.
@@ -20,16 +20,38 @@ RenameGenesSeurat <- function(SeuObj = ls.Seurat[[i]], newnames = HGNC.updated[[
 
 # updateHGNC ------------------------------------------------------------------------------------
 UpdateGenesSeurat <- function(seu, species_="human") { # Update genes symbols that are stored in a Seurat object. It returns a data frame. The last column are the updated gene names.
-  HGNC.updated <- checkGeneSymbols(rownames(seu), unmapped.as.na = FALSE, map = NULL, species = species_)
+  HGNC.updated <- HGNChelper::checkGeneSymbols(rownames(seu), unmapped.as.na = FALSE, map = NULL, species = species_)
   seu <- RenameGenesSeurat(seu, newnames = HGNC.updated)
 }
 
+# Enforce Unique names ------------------------------------------------------------------------------------
+HGNC.EnforceUnique <- function(updatedSymbols) { # Enforce Unique names after HGNC symbol update. updatedSymbols is the output of HGNChelper::checkGeneSymbols.
+    NGL <- updatedSymbols[,3]
+    if (any.duplicated(NGL)) {
+      updatedSymbols[,3] <- make.unique(NGL); "Unique names are enforced by suffixing .1, .2, etc."
+      }
+    return(updatedSymbols)
+}
+x <- HGNC.EnforceUnique(updatedSymbols = SymUpd)
+# While "make.unique" is not the ideal solution, because it generates mismatched, in my integration example it does reduce the mismatching genes from ~800 to 4
 
-# updateHGNC plot ------------------------------------------------------------------------------------
-plot.UpdateStats <- function(genes = HGNC.updated[[i]]) { # Plot the Symbol-update statistics. Works on the data frame returned by `UpdateGenesSeurat()`.
+
+# update stats HGNC  ------------------------------------------------------------------------------------
+GetUpdateStats <- function(genes = HGNC.updated[[i]]) { # Plot the Symbol-update statistics. Works on the data frame returned by `UpdateGenesSeurat()`.
   (MarkedAsUpdated <- genes[genes$Approved == FALSE, ])
   (AcutallyUpdated <- sum(MarkedAsUpdated[,1] != MarkedAsUpdated[,3]))
   (UpdateStats = c((AcutallyUpdated / nrow(genes)), AcutallyUpdated, nrow(genes)))
   return(UpdateStats)
 }
 
+# update stats HGNC plot ------------------------------------------------------------------------------------
+PlotUpdateStats <- function(mat = UpdateStatMat) { # Scatter plot of update stats.
+  HGNC.UpdateStatistics <- mat[, c("percent",  "changed") ]
+  HGNC.UpdateStatistics[, "percent"] <- 100*HGNC.UpdateStatistics[, "percent"]
+  colnames(HGNC.UpdateStatistics) <-  c("Gene Symbols updated (% of total)",  "Number of Gene Symbols updated")
+  lll <- wcolorize(vector = rownames(HGNC.UpdateStatistics))
+  wplot(HGNC.UpdateStatistics, col = lll
+        , xlim = c(0,max(HGNC.UpdateStatistics[,1]))
+        , ylim = c(0,max(HGNC.UpdateStatistics[,2])) )
+  wlegend(NamedColorVec = lll, poz = 1)
+}
