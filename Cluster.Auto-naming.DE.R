e156e3c589e6e48775bfce3e00d052666eaf1bb1
Abel Vertesy <vertesy@users.noreply.github.com>
2020-04-09 18:56:16 +0200

    Cleanup and bugfixes

--- Cluster.Auto-naming.DE.R
+++ Cluster.Auto-naming.DE.R
@@ -5,14 +5,31 @@
 
 # ------------------------------------------------------------------------
 
+# ------------------------------------------------------------------------------------
+StoreTop25Markers <- function(obj = combined.obj # Save the top 25 makers based on `avg_logFC` output table of `FindAllMarkers()` (df_markers) under `@misc$df.markers$res...`. By default, it rounds up insignificant digits up to 3.
+                              , df_markers = df.markers, res = 0.5) {
+  top25.markers <-
+    df_markers %>%
+    group_by(cluster) %>%
+    top_n(n = 25, wt = avg_logFC) %>%
+    dplyr::select(gene) %>%
+    col2named.vec.tbl() %>%
+    splitbyitsnames()
+
+  obj@misc$'top25.markers'[[ppp('res',res)]] <- top25.markers
+  return(obj)
+}
+# combined.obj <- StoreTop25Markers(df_markers = df.markers, res = 0.)
 
 # ------------------------------------------------------------------------------------
 StoreAllMarkers <- function(obj = combined.obj # Save the output table of `FindAllMarkers()` (df_markers) under `@misc$df.markers$res...`. By default, it rounds up insignificant digits up to 3.
                             , df_markers = df.markers, res = 0.5, digit=c(0,3)[2]) {
   if (digit) df_markers[,1:5] <- signif(df_markers[,1:5], digits = digit)
   obj@misc$'df.markers'[[ppp('res',res)]] <- df_markers
+  return(obj)
 }
 # combined.obj <- StoreAllMarkers(df_markers = df.markers, res = 0.5)
+
 
 # ------------------------------------------------------------------------------------
 AutoLabelTop.logFC <- function(obj = combined.obj # Create a new "named identity" column in the metadata of a Seurat object, with `Ident` set to a clustering output matching the `res` parameter of the function. It requires the output table of `FindAllMarkers()`. If you used `StoreAllMarkers()` is stored under `@misc$df.markers$res...`, which location is assumed by default.
@@ -21,7 +38,7 @@ AutoLabelTop.logFC <- function(obj = combined.obj # Create a new "named identity
     df_markers %>%
     group_by(cluster) %>%
     top_n(n = 1, wt = avg_logFC) %>%
-    select(gene) %>%
+    dplyr::select(gene) %>%
     col2named.vec.tbl()
   stopifnot(length(unique(Idents(object = obj))) == length(top.markers))
 
@@ -44,7 +61,7 @@ AutoLabel.KnownMarkers <- function(obj = combined.obj # Create a new "named iden
     df_markers %>%
     arrange(desc(avg_logFC)) %>%
     filter(gene %in%  KnownMarkers) %>%
-    select(avg_logFC, p_val_adj, cluster, gene ) %>%
+    dplyr::select(avg_logFC, p_val_adj, cluster, gene ) %>%
     group_by(gene) %>%
     top_n(n=1, wt=avg_logFC) %>% # Select the top cluster for each gene
     arrange(cluster)
@@ -55,7 +72,7 @@ AutoLabel.KnownMarkers <- function(obj = combined.obj # Create a new "named iden
     matching.clusters %>%
     group_by(cluster) %>% # Select rows with unique values based on column "cluster"
     distinct(cluster,.keep_all = T)  %>%
-    select(gene)
+    dplyr::select(gene)
 
   print("Best matches:")
   print(unique.matches)
@@ -64,7 +81,11 @@ AutoLabel.KnownMarkers <- function(obj = combined.obj # Create a new "named iden
     df_markers %>%
     group_by(cluster) %>%
     top_n(n = 1, wt = (avg_logFC)) %>% # Find the top expressed gene
-    select(gene)
+    dplyr::select(gene)
+
+  top.markers <-
+    top.markers.df %>%
+    col2named.vec.tbl()
 
   missing.annotations <-
     top.markers.df %>%
@@ -95,26 +116,4 @@ DimPlot.ClusterNames <- function(obj = combined.obj # Plot UMAP with Cluster nam
 }
 # DimPlot.ClusterNames()
 
-# ------------------------------------------------------------------------------------
-StoreTop25Markers <- function(obj = combined.obj # Save the top 25 makers based on `avg_logFC` output table of `FindAllMarkers()` (df_markers) under `@misc$df.markers$res...`. By default, it rounds up insignificant digits up to 3.
-                              , df_markers = df.markers, res = 0.5) {
-  top25.markers <-
-    df_markers %>%
-    group_by(cluster) %>%
-    top_n(n = 25, wt = avg_logFC) %>%
-    select(gene) %>%
-    col2named.vec.tbl() %>%
-    splitbyitsnames()
-
-  obj@misc$'top25.markers'[[ppp('res',res)]] <- top25.markers
-}
-# combined.obj <- StoreTop25Markers(df_markers = df.markers, res = 0.)
-
-
-StoreAllMarkers <- function(obj = combined.obj # Save the output table of `FindAllMarkers()` (df_markers) under `@misc$df.markers$res...`. By default, it rounds up insignificant digits up to 3.
-                            , df_markers = df.markers, res = 0.5, digit=c(0,3)[2]) {
-  if (digit) df_markers[,1:5] <- signif(df_markers[,1:5], digits = digit)
-  obj@misc$'df.markers'[[ppp('res',res)]] <- df_markers
-}
-# combined.obj <- StoreAllMarkers(df_markers = df.markers, res = 0.5)
 
