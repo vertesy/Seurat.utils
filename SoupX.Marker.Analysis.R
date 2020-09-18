b5bdee58d01b5de7cb929e24ddce66f0af782f8a
Abel Vertesy <vertesy@users.noreply.github.com>
2020-09-18 15:38:45 +0200

    SoupX marker analysis

---
+++ SoupX.Marker.Analysis.R
@@ -0,0 +1,55 @@
+######################################################################
+# SoupX.Marker.Analysis.R
+######################################################################
+# source ('SoupX.Marker.Analysis.R')
+# rm(list=ls(all.names = TRUE)); try(dev.off(), silent = T)
+
+# Functions ------------------------
+try (source ('~/GitHub/CodeAndRoll/CodeAndRoll.R'),silent= F)
+require('MarkdownReportsDev')
+# source ('~/Github/TheCorvinas/R/DatabaseLinke.r')
+
+# Setup ------------------------
+require(MarkdownReportsDev)
+setup_MarkdownReports(OutDir = "~/Data/POL.GFP/OLD/mtx.broken/SoupX.Marker.Analysis", scriptname = 'SoupX.Marker.Analysis.R')
+
+# Metadata ------------------------
+
+# Parameters ------------------------
+
+
+# Read In ------------------------
+
+
+SoupX.Marker.Analysis = FALSE
+if (SoupX.Marker.Analysis) {
+
+  "Required POL object to be loaded"
+    MarkersFound.123062 <- c('NEAT1', 'FBXL7', 'ZBTB20', 'DACH1', 'AKAP12', 'SEZ6L', 'SLC1A3', 'ZEB1', 'EGR1', 'RORA', 'SAT1'
+                             , 'SVIL', 'FOS', 'GMDS', 'DBI', 'IFITM3', 'LGALS1', 'FABP5', 'CDH4', 'AL589740.1')
+    MarkersFound.123063 <- c('LGALS1', 'IFITM3', 'SPARC', 'TFF3', 'TIMP1', 'FN1', 'IGFBP4', 'ZFP36L1', 'DCN', 'LTBP1'
+                             , 'ANXA2', 'IGFBP3', 'WWTR1', 'ID4', 'CDH11', 'S100A10', 'S100A11', 'MT2A', 'BNC2', 'ID3' )
+
+# ------------------------------------------------------------------------------------------------
+  MarkersFound.SoupX.QuickMarkers <- list(
+    MarkersFound.123062 = MarkersFound.123062,
+    MarkersFound.123063 = MarkersFound.123063
+  )
+  wvenn(MarkersFound.SoupX.QuickMarkers)
+
+
+  multiFeaturePlot.A4(list.of.genes = MarkersFound.123062, obj = combined.obj, subdir =T)
+  multiFeaturePlot.A4(list.of.genes = MarkersFound.123063, obj = combined.obj, subdir =T)
+
+
+}
+
+# QC ------------------------
+
+# ------------------------
+# ------------------------
+# ------------------------
+# ------------------------
+# ------------------------
+
+
