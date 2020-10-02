478f420d14b30833bd3bdf9dc55ccb97f8dc7606
Abel Vertesy <vertesy@users.noreply.github.com>
2020-10-02 10:44:23 +0200

    Load from GitHub

---
+++ Seurat.Utils.Load.from.GitHub.R
@@ -0,0 +1,17 @@
+######################################################################
+# Seurat.Utils.Load.from.GitHub.R
+######################################################################
+# try(source('https://raw.githubusercontent.com/vertesy/Seurat.utils/master/Seurat.Utils.Load.from.GitHub.R'), silent =   T)
+
+try (source("https://raw.githubusercontent.com/vertesy/Seurat.utils/master/Seurat.update.gene.symbols.HGNC.R"))
+try (source("https://raw.githubusercontent.com/vertesy/Seurat.utils/master/metadata.manipulation.R"))
+try (source("https://raw.githubusercontent.com/vertesy/Seurat.utils/master/plotting.dim.reduction.2D.R"))
+try (source("https://raw.githubusercontent.com/vertesy/Seurat.utils/master/plotting.dim.reduction.3D.R"))
+try (source("https://raw.githubusercontent.com/vertesy/Seurat.utils/master/plotting.filtering.R"))
+try (source("https://raw.githubusercontent.com/vertesy/Seurat.utils/master/plotting.statistics.and.QC.R"))
+try (source("https://raw.githubusercontent.com/vertesy/Seurat.utils/master/Read.Write.Save.Load.functions.R"))
+try (source("https://raw.githubusercontent.com/vertesy/Seurat.utils/master/Seurat.object.manipulations.etc.R"))
+try (source("https://raw.githubusercontent.com/vertesy/Seurat.utils/master/Cluster.Auto-naming.DE.R"))
+try (source("https://raw.githubusercontent.com/vertesy/Seurat.utils/master/Seurat.gene.sets.and.GO.terms.R"))
+try (source ('https://raw.githubusercontent.com/vertesy/Seurat.utils/master/MULTI-seq.functions.R'))
+# try (source ('https://raw.githubusercontent.com/vertesy/Seurat.utils/master/Soup.Analysis.of.ambient.RNA.R'))
