5319d323177172df1e47ddea8b3d33c827f7a135
Abel Vertesy <vertesy@users.noreply.github.com>
2020-11-17 14:15:44 +0100

    bugfix, cell metadata was aimed to be replaced by mistake.

--- Seurat.update.gene.symbols.HGNC.R
+++ Seurat.update.gene.symbols.HGNC.R
@@ -15,15 +15,15 @@ UpdateGenesSeurat <- function(obj = ls.Seurat[[i]], species_="human", EnforceUni
 # UpdateGenesSeurat()
 
 # HELPER RenameGenesSeurat  ------------------------------------------------------------------------------------
-RenameGenesSeurat <- function(obj = ls.Seurat[[i]], newnames = HGNC.updated[[i]]$Suggested.Symbol) { # Replace gene names in different slots of a Seurat object. Run this before integration. Run this before integration. It only changes metadata; obj@assays$RNA@counts, @data and @scale.data.
-  print("Run this before integration. It only changes metadata; obj@assays$RNA@counts, @data and @scale.data.")
+RenameGenesSeurat <- function(obj = ls.Seurat[[i]], newnames = HGNC.updated[[i]]$Suggested.Symbol) { # Replace gene names in different slots of a Seurat object. Run this before integration. Run this before integration. It only changes obj@assays$RNA@counts, @data and @scale.data.
+  print("Run this before integration. It only changes obj@assays$RNA@counts, @data and @scale.data.")
   RNA <- obj@assays$RNA
 
-  if (nrow(RNA) == nrow(newnames)) {
+  if (nrow(RNA) == length(newnames)) {
     if (length(RNA@counts)) RNA@counts@Dimnames[[1]]            <- newnames
     if (length(RNA@data)) RNA@data@Dimnames[[1]]                <- newnames
     if (length(RNA@scale.data)) RNA@scale.data@Dimnames[[1]]    <- newnames
-    if (length(obj@meta.data)) rownames(obj@meta.data)          <- newnames
+    # if (length(obj@meta.data)) rownames(obj@meta.data)          <- newnames
   } else {"Unequal gene sets: nrow(RNA) != nrow(newnames)"}
   obj@assays$RNA <- RNA
   return(obj)
