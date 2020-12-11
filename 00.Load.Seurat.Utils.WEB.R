62ba66f4d89ceece58cdfaf0373ff877ba25da82
Abel Vertesy <vertesy@users.noreply.github.com>
2020-12-11 10:49:47 +0100

    move to using sourceGitHub

--- 00.Load.Seurat.Utils.WEB.R
+++ 00.Load.Seurat.Utils.WEB.R
@@ -4,21 +4,22 @@
 # try(source('https://raw.githubusercontent.com/vertesy/Seurat.utils/master/00.Load.Seurat.Utils.WEB.R'), silent =   T)
 # source('~/GitHub/Packages/Seurat.utils/00.Load.Seurat.Utils.WEB.R')
 
-try(source("https://raw.githubusercontent.com/vertesy/Seurat.utils/master/Functions/Seurat.update.gene.symbols.HGNC.R"))
-try(source("https://raw.githubusercontent.com/vertesy/Seurat.utils/master/Functions/Metadata.manipulation.R"))
-try(source("https://raw.githubusercontent.com/vertesy/Seurat.utils/master/Functions/Plotting.dim.reduction.2D.R"))
-try(source("https://raw.githubusercontent.com/vertesy/Seurat.utils/master/Functions/Plotting.dim.reduction.3D.R"))
-try(source("https://raw.githubusercontent.com/vertesy/Seurat.utils/master/Functions/Plotting.filtering.R"))
-try(source("https://raw.githubusercontent.com/vertesy/Seurat.utils/master/Functions/Plotting.statistics.and.QC.R"))
-try(source("https://raw.githubusercontent.com/vertesy/Seurat.utils/master/Functions/Read.Write.Save.Load.functions.R"))
-try(source("https://raw.githubusercontent.com/vertesy/Seurat.utils/master/Functions/Seurat.object.manipulations.etc.R"))
-try(source("https://raw.githubusercontent.com/vertesy/Seurat.utils/master/Functions/Cluster.Auto-naming.DE.R"))
-try(source('https://raw.githubusercontent.com/vertesy/Seurat.utils/master/Functions/MULTI-seq.functions.R'))
-try(source('https://raw.githubusercontent.com/vertesy/Seurat.utils/master/Functions/Jaccard.toolkit.R'))
+sourceGitHub("Seurat.update.gene.symbols.HGNC.R", repo = "Seurat.utils", folder = "Functions")
+sourceGitHub("Metadata.manipulation.R", repo = "Seurat.utils", folder = "Functions")
+sourceGitHub("Plotting.dim.reduction.2D.R", repo = "Seurat.utils", folder = "Functions")
+sourceGitHub("Plotting.dim.reduction.3D.R", repo = "Seurat.utils", folder = "Functions")
+sourceGitHub("Plotting.filtering.R", repo = "Seurat.utils", folder = "Functions")
+sourceGitHub("Plotting.statistics.and.QC.R", repo = "Seurat.utils", folder = "Functions")
+sourceGitHub("Read.Write.Save.Load.functions.R", repo = "Seurat.utils", folder = "Functions")
+sourceGitHub("Seurat.object.manipulations.etc.R", repo = "Seurat.utils", folder = "Functions")
+sourceGitHub("Cluster.Auto-naming.DE.R", repo = "Seurat.utils", folder = "Functions")
+sourceGitHub("MULTI-seq.functions.R", repo = "Seurat.utils", folder = "Functions")
+sourceGitHub("Jaccard.toolkit.R", repo = "Seurat.utils", folder = "Functions")
+
 # try(source('https://raw.githubusercontent.com/vertesy/Seurat.utils/master/Functions/Soup.Analysis.of.ambient.RNA.R'))
 # try (source("https://raw.githubusercontent.com/vertesy/Seurat.utils/master/Functions/Custom.Functions.for.Slingshot.R"))
 
-try(source("https://raw.githubusercontent.com/vertesy/SEO/master/GO-scoring/Seurat.gene.sets.and.GO.terms.R?token=ABG5SV56YUURWCQQ5KZM7I272JHP4"))
+sourceGitHub("Seurat.gene.sets.and.GO.terms.R", repo = "SEO", folder = "GO-scoring", token = "?token=ABG5SV3G3EGGZWSEKGNSE3K73R5F4" )
 
 
 # Requirements ------------------------
