5b02977c2b129057293002597559f81b8b3fcdb0
Abel Vertesy <vertesy@users.noreply.github.com>
2020-12-15 12:37:31 +0100

    ...

--- Functions/Plotting.dim.reduction.2D.R
+++ Functions/Plotting.dim.reduction.2D.R
@@ -143,7 +143,7 @@ multiFeaturePlot.A4 <- function(obj = combined.obj # Save multiple FeaturePlots,
   tictoc::tic()
   ParentDir = OutDir
 
-  if (subdir) create_set_SubDir(... = paste0(foldername,'.', plot.reduction),'/')
+  if (subdir) create_set_SubDir( paste0(foldername,'.', plot.reduction),'/')
   list.of.genes.found = check.genes(list.of.genes = list.of.genes, obj = obj, assay.slot = intersectionAssay)
 
   lsG = iterBy.over(1:length(list.of.genes.found), by = nr.Row * nr.Col)
