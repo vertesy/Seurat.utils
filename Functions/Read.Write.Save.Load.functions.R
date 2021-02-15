1f867854176ba692647ea08f90c5395b7ee50dae
Abel Vertesy <vertesy@users.noreply.github.com>
2021-02-15 16:25:40 +0100

    fun ConvertDropSeqfolders()

--- Functions/Read.Write.Save.Load.functions.R
+++ Functions/Read.Write.Save.Load.functions.R
@@ -39,9 +39,33 @@ Convert10Xfolders <- function(InputDir # Take a parent directory with a number o
     saveRDS(seu, file = fnameOUT)
   }
 }
-
 # Convert10Xfolders(InputDir = InputDir)
 
+# ConvertDropSeqfolders ------------------------------------------------------------------------
+ConvertDropSeqfolders <- function(InputDir # Take a parent directory with a number of subfolders, each containing the standard output of 10X Cell Ranger. (1.) It loads the filtered data matrices; (2.) converts them to Seurat objects, and (3.) saves them as *.RDS files.
+                                  , folderPattern = "SRR*", filePattern = "expression.tsv.gz"
+                                  , min.cells=10, min.features=200, updateHGNC=T, ShowStats=T) {
+  fin <- list.dirs(InputDir, recursive = F)
+  fin <- grepv(x = fin, pattern = folderPattern, perl = F)
+
+  for (i in 1:length(fin)) {
+    pathIN = fin[i]; print(pathIN)
+    fnameIN = basename(fin[i])
+    subdir <- kpps(InputDir, fnameIN)
+    fnameOUT = ppp(subdir, 'min.cells', min.cells, 'min.features', min.features,"Rds")
+
+    CountTable <- list.files(subdir, pattern = filePattern,recursive = F)
+    stopifnot(length(CountTable) == 1 )
+    count_matrix <- readr::read_tsv(file=kpps(subdir, CountTable), )
+    count_matrix <- FirstCol2RowNames(count_matrix)[,-1] # remove 1st "Cell column" # https://github.com/vertesy/SEO/issues/63
+    seu <- CreateSeuratObject(counts = count_matrix, project = fnameIN,
+                              min.cells = min.cells, min.features = min.features)
+    # update----
+    if (updateHGNC) seu <- UpdateGenesSeurat(seu, EnforceUnique = T, ShowStats = T)
+    saveRDS(seu, file = fnameOUT)
+  }
+}
+# ConvertDropSeqfolders(InputDir = InputDir)
 
 # LoadAllSeurats ------------------------------------------------------------------------
 LoadAllSeurats <- function(InputDir # Load all Seurat objects found in a directory. Also works with symbolic links (but not with aliases).
