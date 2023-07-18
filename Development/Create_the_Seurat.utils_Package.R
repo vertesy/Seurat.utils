5a7e69ce8d99037f56c42bc547e5f9f4d86d6f46
vertesy <abel.vertesy@imba.oeaw.ac.at>
2023-07-18 23:40:25 +0200

    ann

--- Development/Create_the_Seurat.utils_Package.R
+++ Development/Create_the_Seurat.utils_Package.R
@@ -20,7 +20,7 @@ require('CodeAndRoll2')
 
 # Setup ------------------------
 PackageName = 	"Seurat.utils"
-package.version = "1.8.8"
+package.version = "1.8.9"
 setwd("~/GitHub/Packages/")
 
 RepositoryDir = kollapse("~/GitHub/Packages/", PackageName, "/")
