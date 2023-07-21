4991838af40794714ab3e77cd159bd6d23e3d560
vertesy <abel.vertesy@imba.oeaw.ac.at>
2023-07-21 23:15:44 +0200

    major reorganization packaged

--- Development/Create_the_Seurat.utils_Package.R
+++ Development/Create_the_Seurat.utils_Package.R
@@ -20,7 +20,7 @@ require('CodeAndRoll2')
 
 # Setup ------------------------
 PackageName = 	"Seurat.utils"
-package.version = "1.9.0"
+package.version = "1.9.2"
 setwd("~/GitHub/Packages/")
 
 RepositoryDir = kollapse("~/GitHub/Packages/", PackageName, "/")
