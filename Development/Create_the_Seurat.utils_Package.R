ba3f9812919cb5274ff6df099b4164c6cdb79f1c
abel.vertesy <abel.vertesy@clip-login-1.cbe.vbc.ac.at>
2023-10-04 19:00:07 +0200

    ...

--- Development/Create_the_Seurat.utils_Package.R
+++ Development/Create_the_Seurat.utils_Package.R
@@ -20,7 +20,7 @@ require('CodeAndRoll2')
 
 # Setup ------------------------
 PackageName = 	"Seurat.utils"
-package.version = "1.9.6"
+package.version = "1.9.7"
 setwd("~/GitHub/Packages/")
 
 RepositoryDir = kollapse("~/GitHub/Packages/", PackageName, "/")
