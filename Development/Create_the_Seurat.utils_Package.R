c0201d1ef7a39ada92f87e85ed1818a2c0ae7f6b
vertesy <abel.vertesy@imba.oeaw.ac.at>
2023-11-26 16:00:21 +0100

    styler

--- Development/Create_the_Seurat.utils_Package.R
+++ Development/Create_the_Seurat.utils_Package.R
@@ -10,42 +10,44 @@ try(dev.off(), silent = TRUE)
 
 
 # Setup ------------------------
-package.name <- 	"Seurat.utils"
+package.name <- "Seurat.utils"
 package.version <- "2.0.9"
 setwd("~/GitHub/Packages/")
 
 RepositoryDir <- paste0("~/GitHub/Packages/", package.name, "/")
-fname <-	paste0(package.name, ".R")
-package.FnP <-		paste0(RepositoryDir, "R/", fname)
+fname <- paste0(package.name, ".R")
+package.FnP <- paste0(RepositoryDir, "R/", fname)
 
 BackupDir <- "~/GitHub/Packages/Seurat.utils/Development/"
 dir.create(BackupDir)
 
-DESCRIPTION <- list("Title" = "Seurat.utils - utility functions for Seurat"
-                    , "Author" = person(given = "Abel", family = "Vertesy", email = "abel.vertesy@imba.oeaw.ac.at", role =  c("aut", "cre") )
-                    , "Authors@R" = 'person(given = "Abel", family = "Vertesy", email = "a.vertesy@imba.oeaw.ac.at", role =  c("aut", "cre") )'
-                    , "Description" = "Seurat.utils Is a collection of utility functions for Seurat v3.
+DESCRIPTION <- list(
+  "Title" = "Seurat.utils - utility functions for Seurat",
+  "Author" = person(given = "Abel", family = "Vertesy", email = "abel.vertesy@imba.oeaw.ac.at", role = c("aut", "cre")),
+  "Authors@R" = 'person(given = "Abel", family = "Vertesy", email = "a.vertesy@imba.oeaw.ac.at", role =  c("aut", "cre") )',
+  "Description" = "Seurat.utils Is a collection of utility functions for Seurat v3.
     Functions allow the automation / multiplexing of plotting, 3D plotting, visualisation of statistics &
-    QC, interaction with the Seurat object, etc. Some functionalities require functions from CodeAndRoll and MarkdownReports libraries."
-                    , "License" = "GPL-3 + file LICENSE"
-                    , "Version" = package.version
-                    , "Packaged" =  Sys.time()
-                    # , "Repository" =  "CRAN"
-                    , "Depends" =  "ggplot2, Seurat, Stringendo, CodeAndRoll2, ggExpress"
-                    , "Imports" = "cowplot, dplyr, ggcorrplot, ggpubr, ggrepel, graphics, grDevices, HGNChelper,
+    QC, interaction with the Seurat object, etc. Some functionalities require functions from CodeAndRoll and MarkdownReports libraries.",
+  "License" = "GPL-3 + file LICENSE",
+  "Version" = package.version,
+  "Packaged" = Sys.time()
+  # , "Repository" =  "CRAN"
+  , "Depends" = "ggplot2, Seurat, Stringendo, CodeAndRoll2, ggExpress",
+  "Imports" = "cowplot, dplyr, ggcorrplot, ggpubr, ggrepel, graphics, grDevices, HGNChelper,
                           htmlwidgets, MarkdownHelpers, MarkdownReports, Matrix, matrixStats, princurve, ReadWriter, pheatmap,
                           R.utils, readr, reshape2, scales, Seurat, SoupX, sparseMatrixStats, stringr, tibble, tictoc,
-                          vroom, job, qs"
-                    , "Suggests" = "SoupX, princurve, EnhancedVolcano"
-                    , "BugReports"= "https://github.com/vertesy/Seurat.utils/issues"
+                          vroom, job, qs",
+  "Suggests" = "SoupX, princurve, EnhancedVolcano",
+  "BugReports" = "https://github.com/vertesy/Seurat.utils/issues"
 )
 
 
 setwd(RepositoryDir)
-if ( !dir.exists(RepositoryDir) ) { create(path = RepositoryDir, description = DESCRIPTION, rstudio = TRUE)
+if (!dir.exists(RepositoryDir)) {
+  create(path = RepositoryDir, description = DESCRIPTION, rstudio = TRUE)
 } else {
   getwd()
-  try(file.remove(c("DESCRIPTION","NAMESPACE"))) # , "Seurat.utils.Rproj"
+  try(file.remove(c("DESCRIPTION", "NAMESPACE"))) # , "Seurat.utils.Rproj"
   usethis::create_package(path = RepositoryDir, fields = DESCRIPTION, open = F)
 }
 
@@ -58,8 +60,8 @@ if ( !dir.exists(RepositoryDir) ) { create(path = RepositoryDir, description = D
 # RoxygenReady(package.FnP)
 
 # replace output files ------------------------------------------------
-BackupOldFile <-	paste0(BackupDir, "Development", ".bac")
-AnnotatedFile <-	paste0(BackupDir, "Development", ".annot.R")
+BackupOldFile <- paste0(BackupDir, "Development", ".bac")
+AnnotatedFile <- paste0(BackupDir, "Development", ".annot.R")
 file.copy(from = package.FnP, to = BackupOldFile, overwrite = TRUE)
 # file.copy(from = AnnotatedFile, to = package.FnP, overwrite = TRUE)
 
@@ -74,9 +76,11 @@ warnings()
 
 {
   "update cff version"
-  citpath <- paste0(RepositoryDir, 'CITATION.cff')
-  xfun::gsub_file(file = citpath, perl = T
-                  , "^version: v.+", paste0("version: v", package.version))
+  citpath <- paste0(RepositoryDir, "CITATION.cff")
+  xfun::gsub_file(
+    file = citpath, perl = T,
+    "^version: v.+", paste0("version: v", package.version)
+  )
 }
 
 
@@ -103,13 +107,15 @@ check(RepositoryDir, cran = TRUE)
 
 # Check package dependencies ------------------------------------------------
 {
-  depFile = paste0(RepositoryDir, 'Development/Dependencies.R')
+  depFile <- paste0(RepositoryDir, "Development/Dependencies.R")
 
   (f.deps <- NCmisc::list.functions.in.file(filename = package.FnP))
-  clipr::write_clip( f.deps$`character(0)`)
+  clipr::write_clip(f.deps$`character(0)`)
 
-  sink(file = depFile); print(f.deps); sink()
-  p.deps <- gsub(x = names(f.deps), pattern = 'package:', replacement = '')
+  sink(file = depFile)
+  print(f.deps)
+  sink()
+  p.deps <- gsub(x = names(f.deps), pattern = "package:", replacement = "")
   write(x = p.deps, file = depFile, append = T)
 }
 
@@ -118,7 +124,7 @@ check(RepositoryDir, cran = TRUE)
 # Package styling, and visualization ------------------------------------------------
 {
   styler::style_pkg(RepositoryDir)
-  styler::style_file("~/GitHub/Packages/Seurat.utils/Development/Create_the_Seurat.utils_Package.R")
+  # styler::style_file("~/GitHub/Packages/Seurat.utils/Development/Create_the_Seurat.utils_Package.R")
 
   {
     # Exploring the Structure and Dependencies of my R Package:
@@ -140,7 +146,6 @@ check(RepositoryDir, cran = TRUE)
 
 
 if (F) {
-
   findFunctionOrigin <- function(function_names) {
     # Get a list of all installed packages
     installed_packages <- rownames(installed.packages())
@@ -157,7 +162,7 @@ if (F) {
 
       # Check each package
       for (pkg in installed_packages) {
-        cat('.', append = T)
+        cat(".", append = T)
         # Check if the package contains the function
         if (func_name %in% ls(getNamespace(pkg), all.names = TRUE)) {
           found_in_packages <- c(found_in_packages, pkg)
@@ -172,8 +177,7 @@ if (F) {
     return(function_origins)
   }
 
-  FunctionOrigins <- findFunctionOrigin( f.deps$`character(0)`)
-
+  FunctionOrigins <- findFunctionOrigin(f.deps$`character(0)`)
 }
 
 # NCmisc::list.functions.in.file(package.FnP)
