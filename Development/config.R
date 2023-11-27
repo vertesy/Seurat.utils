6622ac5cd77a96e5e775ff83dee9f1e11bd1af6b
vertesy <abel.vertesy@imba.oeaw.ac.at>
2023-11-27 17:16:33 +0100

    new pac

--- Development/config.R
+++ Development/config.R
@@ -5,12 +5,13 @@ DESCRIPTION <- list(
   title = "Seurat.utils - utility functions for Seurat",
   description = "Seurat.utils Is a collection of utility functions for Seurat v3.
     Functions allow the automation / multiplexing of plotting, 3D plotting, visualisation of statistics &
-    QC, interaction with the Seurat object, etc. Some functionalities require functions from CodeAndRoll and MarkdownReports libraries.",
+    QC, interaction with the Seurat object, etc. Some functionalities require functions from
+    CodeAndRoll and MarkdownReports libraries.",
   depends = "ggplot2, Seurat, Stringendo, CodeAndRoll2, ggExpress, magrittr",
   imports = "cowplot, Seurat, ReadWriter, dplyr, ggcorrplot, ggpubr, ggrepel, grDevices, HGNChelper,
-                          htmlwidgets, MarkdownHelpers, MarkdownReports, Matrix, matrixStats, princurve, pheatmap,
-                          R.utils, readr, reshape2, scales, SoupX, sparseMatrixStats, stringr, tibble, tictoc,
-                          vroom, job, qs, foreach, tidyverse",
+    htmlwidgets, MarkdownHelpers, MarkdownReports, Matrix, matrixStats, princurve, pheatmap,
+    R.utils, readr, reshape2, scales, SoupX, sparseMatrixStats, stringr, tibble, tictoc,
+    vroom, job, qs, foreach, tidyverse",
   suggests = "SoupX, princurve, EnhancedVolcano",
 
   author.given = "Abel",
