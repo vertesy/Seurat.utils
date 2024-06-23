059cb985540b3d58df01af7ed3487e18a6bba8ed
vertesy <abel.vertesy@imba.oeaw.ac.at>
2024-06-23 10:19:51 +0200

    nf xread set m

--- R/Seurat.Utils.R
+++ R/Seurat.Utils.R
@@ -4700,9 +4700,10 @@ xread <- function(file, nthreads = 4,
 
     if (set_m) {
       # if (!exists("m")) {
-        m <- list.fromNames(colnames(obj@meta.data))
+        # m <- list.fromNames(colnames(obj@meta.data))
+        m <- lapply(data.frame(obj@meta.data), function(x) head(unique(x), 100))
         assign("m", m, envir = .GlobalEnv)
-        message("Variable 'm', a list of @meta.data colnames, is now defined in the global environment.")
+        message("Variable 'm', a list of @meta.data colnames and first 100 uq values, is now defined in the global environment.")
       # } else {
       #   message("Variable 'm' already exists in the global environment, not overwritten")
       # } # exists("m")
